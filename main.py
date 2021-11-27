ba97677dc3b535b623e13d55855885be6314296f
bessasjr <bessasjr@gmail.com>
2021-11-26 21:04:42 -0300

    init_proj

---
+++ main.py
@@ -0,0 +1,15 @@
+from pynput import mouse
+import pyautogui
+
+
+def on_click(x, y, button, pressed):
+    if pressed is True:
+        r,g,b = pyautogui.pixel(x, y)
+        rgb = (f'{r}, {g}, {b}')
+        rgbhex = '#%02x%02x%02x' % (r,g,b)
+
+        print(rgb)
+        print(rgbhex,'\n')
+
+with mouse.Listener(on_click=on_click) as listener:
+    listener.join()
