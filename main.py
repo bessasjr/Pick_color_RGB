17dd7a517e3564b1c59fff95b703207b8e3e08f2
Junior Bessas <93383793+bessasjr@users.noreply.github.com>
2022-06-14 16:02:39 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pyautogui
 
 
 def on_click(x, y, button, pressed):
-    if pressed is True:
+    if button == mouse.Button.left and pressed:
         r,g,b = pyautogui.pixel(x, y)
         rgb = (f'{r}, {g}, {b}')
         rgbperc = f'{r/ 255:.3f},{g/ 255:.3f},{b/ 255:.3f}'
@@ -13,5 +13,14 @@ def on_click(x, y, button, pressed):
         print(f'Percentual: {rgbperc}')
         print(f'Hexadecimal: {rgbhex}\n')
 
+    
+    if button == mouse.Button.right and pressed:
+        listener.stop()
+        
+        
 with mouse.Listener(on_click=on_click) as listener:
-    listener.join()
+    listener.join() 
+
+
+
+    
